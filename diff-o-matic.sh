7ba8afc136f08c96f5a789445db2d2e039cddd06
Honie Lui <honie.lui@moovweb.com>
2012-11-15 14:44:48 -0800

    let's date and then sleep

--- diff-o-matic.sh
+++ diff-o-matic.sh
@@ -4,8 +4,9 @@ while [ "$diff" = "" ]
 do
   let "i = i + 1"
   echo "Diff-o-matic try #$i"
+  date
   `curl $1  > out1 2> /dev/null`
   diff=`diff out1 out2`
+  sleep 60
 done
-sudo rm -rf ~/workspace/briangleshackathon
 echo "Error, pages don't match"
